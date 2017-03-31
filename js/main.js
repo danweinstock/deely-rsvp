436b9d25822a98066a9f1ab019b550dc25607d0c
danweinstock <dan.weinstock@yahoo.com>
2017-03-30 21:53:36 -0400

    optimized images and minimized cssand js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
-$(document).ready(function(){
-		console.log("Purple Monkey Dishwasher");
-		$('.nav-item').on('click', function(e){
-			e.preventDefault();
-			var option = $(this).attr('href');
-			console.log(option);
-			$("html, body").animate({ scrollTop: $(option).offset().top-50}, 1000);
-		});
-});
+// $(document).ready(function(){
+// 		console.log("Purple Monkey Dishwasher");
+// 		$('.nav-item').on('click', function(e){
+// 			e.preventDefault();
+// 			var option = $(this).attr('href');
+// 			console.log(option);
+// 			$("html, body").animate({ scrollTop: $(option).offset().top-50}, 1000);
+// 		});
+// });
+$(document).ready(function(){console.log("Purple Monkey Dishwasher"),$(".nav-item").on("click",function(o){o.preventDefault();var e=$(this).attr("href");console.log(e),$("html, body").animate({scrollTop:$(e).offset().top-50},1e3)})});
