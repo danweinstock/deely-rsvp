6ca2d278f3a598ebe65fac1d83ae260d674d19d6
danweinstock <dan.weinstock@yahoo.com>
2017-04-05 16:16:05 -0400

    changed nav to disappear on click for mobile

--- js/main.js
+++ js/main.js
@@ -6,5 +6,8 @@
 // 			console.log(option);
 // 			$("html, body").animate({ scrollTop: $(option).offset().top-50}, 1000);
 // 		});
+
 // });
+// });
+
 $(document).ready(function(){console.log("Purple Monkey Dishwasher"),$(".nav-item").on("click",function(o){o.preventDefault();var e=$(this).attr("href");console.log(e),$("html, body").animate({scrollTop:$(e).offset().top-50},1e3)})});
